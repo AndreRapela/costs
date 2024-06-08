2571bc338cb01e1e4ef41fad07412586496ad4a8
André Rapela <andre.d.q.rapela@gmail.com>
2024-06-08 17:37:08 -0300

    Footer finalizado

--- src/App.js
+++ src/App.js
@@ -5,25 +5,24 @@ import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 
 import Container from './components/layout/Container'
+import NavBar from './components/layout/NavBar'
+import Footer from './components/layout/Footer'
+import Projects from './components/pages/Projects'
 
 function App() {
   return (
    <Router>
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/company">Empresa</Link>
-      <Link to="/contact">Contato</Link>
-      <Link to="/newproject">Novo projeto</Link>
-    </div>
-    <Container>
+    <NavBar/>
+    <Container customClass = 'min_height'>
     <Routes>
     <Route exact path='/' element={<Home/>} /> 
     <Route exact path='/company' element={<Company/>} /> 
     <Route exact path='/contact' element={<Contact/>} /> 
     <Route exact path='/newproject' element={<NewProject/>} /> 
+    <Route exact path='/projects' element={<Projects/>} /> 
     </Routes>
     </Container>
-    <p>Footer</p>
+    <Footer />
    </Router>
   );
 }
